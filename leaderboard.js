5c97959bd595fc87ff49f7a9efc772ba0b241a30
cherrerog <c.herrerog@alumnos.urjc.es>
2015-10-19 18:55:13 +0200

    leaderboard.jasmine inicio

---
+++ leaderboard.js
@@ -0,0 +1,70 @@
+
+PlayersList = new Mongo.Collection('players');
+
+
+
+if (Meteor.isClient) {
+  Template.leaderboard.helpers({
+    'player': function(){
+        return PlayersList.find({},{sort:{score: -1,name:1}});
+
+    },
+    'selectedClass': function(){
+   	 	var playerId = this._id;
+   	 	var selectedPlayer = Session.get('selectedPlayer');
+   	 	if(playerId == selectedPlayer){
+       		return "selected"
+    	}
+   	 	
+	},
+	'showSelectedPlayer': function(){
+    var selectedPlayer = Session.get('selectedPlayer');
+    return PlayersList.findOne(selectedPlayer)
+	}
+
+  
+});
+  Template.leaderboard.events({
+    'click .player': function(){
+    	var playerId = this._id;
+    	Session.set('selectedPlayer', playerId);
+        //var selectedPlayer = Session.get('selectedPlayer');
+        //console.log(selectedPlayer);
+
+    },
+	'click .increment': function(){
+    	var selectedPlayer = Session.get('selectedPlayer');
+    	PlayersList.update(selectedPlayer, {$inc: {score: 5}});
+
+	},
+	'click .decrement': function(){
+    	var selectedPlayer = Session.get('selectedPlayer');
+    	PlayersList.update(selectedPlayer, {$inc: {score: -5}});
+
+	},
+	'click .remove': function(){
+    var selectedPlayer = Session.get('selectedPlayer');
+    PlayersList.remove(selectedPlayer);
+	}
+
+});
+  Template.addPlayerForm.events({
+    'submit form': function(event){
+    event.preventDefault();
+    var playerNameVar = event.target.playerName.value;
+    PlayersList.insert({
+        name: playerNameVar,
+        score: 0
+    });
+}
+
+});
+
+
+
+  
+}
+
+if (Meteor.isServer) {
+  
+}
